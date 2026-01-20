63750d64c5ffa81c81d082e8a3cb5031fb049d60
Ruben van der Linde <ruben@conduction.nl>
2026-01-20 12:01:33 +0100

    Bugfix: Wrong schemas

--- lib/Controller/RegistersController.php
+++ lib/Controller/RegistersController.php
@@ -306,9 +306,11 @@ class RegistersController extends Controller
                                 $this->logger->debug("RegistersController: No count for schema {$schemaId}, set to 0");
                             }
                         }
+                        unset($schema); // CRITICAL: Unset reference to prevent corruption of array in subsequent iterations.
                     }
                 }
             }
+            unset($register); // CRITICAL: Unset reference to prevent array corruption.
         }
 
         // If '@self.stats' is requested, attach statistics to each register.
