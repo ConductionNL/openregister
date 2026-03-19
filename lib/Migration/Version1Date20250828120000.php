e3e0ffeb3d0738b431e6b5845e92ff275cbfc351
Robert Zondervan <robert@conduction.nl>
2026-03-19 15:42:15 +0100

    Fix CS issues

--- lib/Migration/Version1Date20250828120000.php
+++ lib/Migration/Version1Date20250828120000.php
@@ -40,10 +40,10 @@ use OCP\Migration\SimpleMigrationStep;
  */
 class Version1Date20250828120000 extends SimpleMigrationStep
 {
-	/**
-	 * @param IDBConnection $connection The database connection
-	 * @param IConfig $config The configuration interface
-	 */
+    /**
+     * @param IDBConnection $connection The database connection
+     * @param IConfig       $config     The configuration interface
+     */
     public function __construct(
         private readonly IDBConnection $connection,
         private readonly IConfig $config,
@@ -65,7 +65,7 @@ class Version1Date20250828120000 extends SimpleMigrationStep
      */
     public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper
     {
-        /**
+        /*
          * @var ISchemaWrapper $schema
          */
 
