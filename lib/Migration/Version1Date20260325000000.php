8a2d79123b8f315bd94c2ade978a3354c19d7933
Ruben van der Linde <rubenvdlinde@gmail.com>
2026-03-25 08:05:19 +0100

    feat: Add tmlo JSON column to ObjectEntity for TMLO archival metadata
    - Add database migration Version1Date20260325000000 for tmlo column
    - Add tmlo property with JSON type to ObjectEntity
    - Include tmlo in getObjectArray serialization and getter defaults
    - Closes #1135, closes #1136, closes #1137

---
+++ lib/Migration/Version1Date20260325000000.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Database migration to add `tmlo` column to the openregister_objects table.
+ *
+ * Adds a JSON column for storing TMLO (Toepassingsprofiel Metadatastandaard
+ * Lokale Overheden) archival metadata on objects.
+ *
+ * @category Migration
+ * @package  OCA\OpenRegister\Migration
+ *
+ * @author  Conduction Development Team <dev@conduction.nl>
+ * @license EUPL-1.2 https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * @link https://OpenRegister.app
+ */
+
+declare(strict_types=1);
+
+namespace OCA\OpenRegister\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\DB\Types;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Adds the `tmlo` JSON column to the openregister_objects table.
+ *
+ * This column stores TMLO-compliant archival metadata including:
+ * classificatie, archiefnominatie, archiefactiedatum, archiefstatus,
+ * bewaarTermijn, and vernietigingsCategorie.
+ *
+ * @package OCA\OpenRegister\Migration
+ */
+class Version1Date20260325000000 extends SimpleMigrationStep
+{
+
+    /**
+     * Change the database schema.
+     *
+     * @param IOutput $output        Migration output
+     * @param Closure $schemaClosure Schema closure
+     * @param array   $options       Migration options
+     *
+     * @return ISchemaWrapper|null The updated schema or null if no changes
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper
+    {
+        // Get the schema wrapper from the closure.
+        $schema = $schemaClosure();
+
+        $tableName = 'openregister_objects';
+
+        if ($schema->hasTable($tableName) === false) {
+            $output->info("Table {$tableName} does not exist, skipping migration");
+            return null;
+        }
+
+        $table = $schema->getTable($tableName);
+
+        if ($table->hasColumn('tmlo') === true) {
+            $output->info("Column 'tmlo' already exists in {$tableName}, skipping");
+            return null;
+        }
+
+        $table->addColumn(
+            'tmlo',
+            Types::TEXT,
+            [
+                'notnull' => false,
+                'default' => null,
+            ]
+        );
+
+        $output->info("Added 'tmlo' column to {$tableName}");
+
+        return $schema;
+    }//end changeSchema()
+
+
+}//end class
